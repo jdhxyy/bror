8a87fa967fe6d09d83f74e47339a5990efa1f737
jdhxyy <jdh821@163.com>
2021-08-09 23:23:10 +0800

    Defect:REQ039.001 Modify:zhouxin Desc:新建

---
+++ bror.h
@@ -0,0 +1,15 @@
+// Copyright 2021-2021 The jdh99 Authors. All rights reserved.
+// 操作系统中间件.屏蔽操作系统的差异,提供统一的API
+// Authors: jdh99 <jdh821@163.com>
+
+#ifndef BROR_H
+#define BROR_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+// BrorFunc 入口函数类型
+typedef void (*BrorStart)(void);
+
+#endif
